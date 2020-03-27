6d26a3a0902e7801d79918ea15bcc44e83967b97
DutchDS <dutchdatasmarts@gmail.com>
2020-03-27 12:38:13 -0500

    South Korea and Active Data fix

--- static/sql/schema.sql
+++ static/sql/schema.sql
@@ -328,3 +328,10 @@ from covid_data_4
 group by country_region, short_date
 order by short_date
 )
+****************************************************************************************************
+drop view if exists states_data_ext_v;
+create view states_data_ext_v as (
+Select min(short_date), sv.emergency_date, sv.state, sv.state_name 
+from covid_by_state_v sv join states_data s on sv.state = s.state
+group by sv.emergency_date, sv.state, sv.state_name
+)
