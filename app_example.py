9d700939f1186e815bb438bb3bf4aa79042d84d8
DutchDS <dutchdatasmarts@gmail.com>
2020-04-02 11:06:01 -0500

    Working with FLASK!

--- app_example.py
+++ app_example.py
@@ -23,16 +23,103 @@ engine = create_engine(DATABASE_URI)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI 
 
 db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/corona_db"
 
+# db = SQLAlchemy(app)
 @app.route("/")
 def world():
+    # result_set = []
+    
+    # query_str = open('static/sql/world_top_10.sql')
+    # query_text = ""
+    
+    # for text in query_str:
+    #     query_text = query_text + text
+        
+    # result_set = engine.execute(query_text)
 
-    return render_template("Covid_Landing.html")
+    # result_sum = []
 
-@app.route("/home")
-def home():
+    # query_str = open('static/sql/world_summary.sql')
+    # query_text = ""
+    
+    # for text in query_str:
+    #     query_text = query_text + text
 
-    return render_template("Covid_Home.html")
+    # result_sum = engine.execute(query_text)
+
+    # max_date = []
+
+    # query_str = open('static/sql/world_max_date.sql')
+    # query_text = ""
+    
+    # for text in query_str:
+    #     query_text = query_text + text
+
+    # max_date = engine.execute(query_text)
+
+    # counter = 0
+
+    # query_str = open('static/sql/world_days.sql')
+    # query_text = ""
+    
+    # for text in query_str:
+    #     query_text = query_text + text
+
+    # result_count = engine.execute(query_text)
+
+    # for i in result_count:
+    #     counter = i[0]
+
+    # return render_template("world.html", world_facts=result_set, world_sum=result_sum, world_max_date=max_date, world_counter=counter)
+    return render_template("world.html")
+
+@app.route("/USA")
+def USA():
+    result_set = []
+    
+    query_str = open('static/sql/USA_top_10.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+        
+    result_set = engine.execute(query_text)
+
+    result_sum = []
+
+    query_str = open('static/sql/USA_summary.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+
+    result_sum = engine.execute(query_text)
+
+    max_date = []
+
+    query_str = open('static/sql/world_max_date.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+
+    max_date = engine.execute(query_text)
+
+    counter = 0
+
+    query_str = open('static/sql/world_days.sql')
+    query_text = ""
+    
+    for text in query_str:
+        query_text = query_text + text
+
+    result_count = engine.execute(query_text)
+
+    for i in result_count:
+        counter = i[0]
+
+    return render_template("USA.html", world_facts=result_set, world_sum=result_sum, world_max_date=max_date, world_counter=counter)
 
 @app.route("/china")
 def china(): 
