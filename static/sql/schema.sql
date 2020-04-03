2594aa3dd226720f6bf9371b3eca68e65d9d583e
DutchDS <dutchdatasmarts@gmail.com>
2020-04-03 08:23:31 -0500

    fix_script.sql

--- static/sql/schema.sql
+++ static/sql/schema.sql
@@ -64,21 +64,29 @@ DROP VIEW IF EXISTS covid_by_county_v CASCADE;
 DROP VIEW IF EXISTS covid_by_state_v CASCADE;
 DROP VIEW IF EXISTS covid_by_country_v CASCADE;
 
-DROP TABLE IF EXISTS covid_data_states;
+DROP TABLE IF EXISTS covid_data_states CASCADE;
 
 CREATE TABLE covid_data_states
 (
 	id serial not null,	
     date date,
     state text COLLATE pg_catalog."default",
-    positive bigint,
+    positive bigint, 
     negative bigint,
     pending bigint,
+	hospitalized_curr bigint,
+	hospitalized_cum bigint,
+	"in_ICU_curr" bigint,
+	"in_ICU_cum" bigint,
+	on_vent_curr bigint,
+	on_vent_cum bigint,
+	recovered bigint,
     hospitalized bigint,
     death bigint,
     dontuse_total bigint,
     check_date date,
     total_tests bigint,
+	pos_neg bigint,
     fips bigint,
     death_inc bigint,
     hospital_inc bigint,
@@ -395,7 +403,10 @@ DROP VIEW IF EXISTS covid_and_census_by_state_v;
 create view covid_and_census_by_state_v as (
 select 
 	date, s.state_name, s.state, s.emergency_date, s.first_case_date, 
-	positive, negative, pending, hospitalized, death, (positive + hospitalized + death) as total_cases, total_tests, death_inc, hospital_inc, neg_inc, pos_inc, tot_tests_inc, 
+	positive, negative, pending, hospitalized, death, (positive + hospitalized + death) as total_cases, total_tests,
+	hospitalized_curr, hospitalized_cum, "in_ICU_curr" as in_ICU_curr, "in_ICU_cum" as in_ICU_cum,
+	on_vent_curr, on_vent_cum, recovered,
+	death_inc, hospital_inc, neg_inc, pos_inc, tot_tests_inc, 
 	s.latitude, s.longitude,
 	cd.total_pop, cd.men, cd.women, cd.white, cd.black, cd.hispanic, cd.native, cd.pacific
 from covid_data_states c 
